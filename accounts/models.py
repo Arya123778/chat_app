858d8df70db60c9b9f987584f90e2030543249ba
Arya <aryasandbhor03@gmail.com>
2026-02-26 18:58:34 +0530

    commit accounts app models.py code

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
-from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db import models 
 # Create your models here.
+class User(AbstractUser):
+    email=models.EmailField(unique=True)
+    bio=models.TextField(blank=True, null=True)
+    avatar=models.ImageField(upload_to='avatars/', blank=True, null=True)
+    is_online=models.BooleanField(default=False)
+    
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username']
